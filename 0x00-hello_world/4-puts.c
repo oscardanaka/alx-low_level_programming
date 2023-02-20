d94ff777ab3d344dd20748480f060c4adb91bafa
Oscar Eyanohonre <tegaeyanohonre@gmail.com>
2023-02-20 14:54:59 -0800

    puts test for betty compliance

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a file with puts gn
+ * Return: 0
+ */
+int main(void)
+{
+puts ("\"Proramming is like building a multilingual puzzle");
+return (0);
+}
